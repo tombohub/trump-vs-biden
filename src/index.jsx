648434f09efd5d124e289be1103f17b0656fc878
babaroga <mama@mia.com>
2020-09-09 12:43:43 -0400

    snapshot flow

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,15 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import * as cheerio from "cheerio";
-import * as axios from "axios";
+import { render } from "react-snapshot";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
 
 // If you want your app to work offline and load faster, you can change
