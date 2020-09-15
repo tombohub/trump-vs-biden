ab0bb13c645e1301c97bafd7a824e18f4a0ec1e9
babaroga <mama@mia.com>
2020-09-15 15:57:20 -0400

    Updates

---
+++ precache-manifest.82e13e61d07bb06f3d4f787f0e63a532.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab7562bfab8bdfc138d532bca3d86b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc1c201cb9bc6bdf1b37",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "c6d7caa21809ca88f76a",
+    "url": "/static/css/main.68a1e400.chunk.css"
+  },
+  {
+    "revision": "dc1c201cb9bc6bdf1b37",
+    "url": "/static/js/2.49b06643.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.49b06643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d7caa21809ca88f76a",
+    "url": "/static/js/main.7d0034b2.chunk.js"
+  },
+  {
+    "revision": "2dfa0395b03bd3cff9fe",
+    "url": "/static/js/runtime-main.108fb5c7.js"
+  },
+  {
+    "revision": "f545efb1f99b3846b04ddb4f1187655f",
+    "url": "/static/media/biden.f545efb1.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "642813072297273c373b9fbc07b7b6bd",
+    "url": "/static/media/trump.64281307.png"
+  }
+]);
